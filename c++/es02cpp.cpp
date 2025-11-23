c5aeccf958314bcd1deef3f115b3c1c4eda5462d
Sisco <gmarchese@stevejobs.academy>
2025-11-23 09:20:04 +0100

    Add files via upload

---
+++ c++/es02cpp.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Classe base
+class Persona {
+protected:
+    string nome;
+    int eta;
+
+public:
+    void inserisciDati() {
+        cout << "Inserisci il nome: ";
+        getline(cin, nome);  // legge anche spazi nel nome
+        cout << "Inserisci l'eta': ";
+        cin >> eta;
+        cin.ignore(); // pulisce il buffer dopo cin
+    }
+
+    void mostraDati() const {
+        cout << "Nome: " << nome << endl;
+        cout << "Eta': " << eta << endl;
+    }
+};
+
+// Classe derivata
+class Lavoratore : public Persona {
+private:
+    double stipendio;
+
+public:
+    void inserisciDatiLavoratore() {
+        inserisciDati(); // chiama la funzione della classe base
+        cout << "Inserisci lo stipendio: ";
+        cin >> stipendio;
+    }
+
+    void mostraDatiLavoratore() const {
+        mostraDati();
+        cout << "Stipendio: " << stipendio << " $" << endl;
+    }
+};
+
+int main() {
+    Lavoratore l;
+    //Persona p;
+    //p.inserisciDati();
+    //p.mostraDati();
+    
+    l.inserisciDatiLavoratore();
+    cout << "\n--- DATI DEL LAVORATORE ---" << endl;
+    l.mostraDatiLavoratore();
+
+    return 0;
+}
+
---
+++ c++/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/* C++ */
+
+int main(){
+  cout << "\nHello World!!! "<< endl;
+  return 0;
+}
